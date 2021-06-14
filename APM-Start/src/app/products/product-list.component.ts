382a02603c2f7c1e96be45389ba2c062f51b159d
Jon Stump <jmstump@gmail.com>
2021-06-14 12:54:58 -0700

    add filter for products using getters and setters

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -12,7 +12,18 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+
+  private _listFilter: string = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter:', value);
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  filteredProducts: IProduct[] = [];
   products: IProduct[] = [
     {
       'productId': 1,
@@ -66,11 +77,16 @@ export class ProductListComponent implements OnInit {
     }
   ];
 
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+                               product.productName.toLocaleLowerCase().includes(filterBy));
+  }
   toggleImage(): void {
   this.showImage = !this.showImage;
   }
 
   ngOnInit(): void {
-    console.log('In OnInit');
+    this.listFilter = 'cart';
   }
 }
